08b9aa0ffbf4dd3b13c5fa861db87940557ba07e
Daniel <danieldiazgonza52@gmail.com>
2021-12-02 01:51:22 -0500

    Little change

--- front-page.php
+++ front-page.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-12">
                 <h1>Front Page</h1>
-                <p>This is the front page of my theme.</p>
+                <p>This is the front page of my theme. More text</p>
             </div>
         </div>
     </div>
